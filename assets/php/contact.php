9ecd756e2388ed2911676accc2c6656cd6e28281
mmiric <marijanamiric@Marijanas-MacBook-Pro.local>
2017-03-18 23:02:32 -0400

    contact form

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -5,7 +5,7 @@
 	$subject = "Website Contact Form";
 
 	// You can put here your email
-	$header = "From: noreply@yoursite.com\r\n";
+	$header = "From: marijana.miric@gmail.com\r\n";
 	$header.= "MIME-Version: 1.0\r\n";
 	$header.= "Content-Type: text/plain; charset=utf-8\r\n";
 	$header.= "X-Priority: 1\r\n";
